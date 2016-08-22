a115bd62d73c9da8146b86b622082a004bd510c1
poonamk <pkholkar22@gmail.com>
2016-08-22 17:44:11 +0530

    added html file

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = express();
 
 app.get('/',function(req,res) {
   console.log("Hello World");
-  res.send("Hello World");
+  //res.send("Hello World");
+  res.sendFile("./angular-frontend/HelloWorld.html",{"root": __dirname});
 });
 
 console.log("server running at port : 4000");
