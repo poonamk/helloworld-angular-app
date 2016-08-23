fca9f46ee33c3b4b6894c9a2c68c6344952c5feb
poonamk <pkholkar22@gmail.com>
2016-08-23 11:17:36 +0530

    added deployment script

--- startnode.bat
+++ startnode.bat
@@ -1,2 +1,8 @@
-cd C:\Users\poonam_kholkar\Desktop\app\helloworld-angular-app
-npm start
+#cd C:\Users\poonam_kholkar\Desktop\app\helloworld-#angular-app
+#npm start
+echo "deploy script"
+E:
+cd Deployment
+git init
+git pull 
+npm install
