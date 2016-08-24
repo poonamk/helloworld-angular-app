d72cc491fff7de8c3dfd3832b0e25e214b0d8eef
poonamk <pkholkar22@gmail.com>
2016-08-24 11:46:58 +0530

    jshint added

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),    
+	jshint: {
+            all: ['Gruntfile.js','angular-frontend/src/superb.js','angular-frontend/src/impressive.js']
+        },
     concat: {
       build: {
         files: {
@@ -28,6 +31,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
  // grunt.registerTask('default', ['jshint','clean']);
