a81b009eacd3cbbe93b90231a9ab0541be7f59be
poonamk <pkholkar22@gmail.com>
2016-08-24 11:13:30 +0530

    added new files

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),    
+    concat: {
+      build: {
+        files: {
+          'angular-frontend/dist/<%= pkg.name %>.js': [
+            'angular-frontend/src/superb.js',
+            'angular-frontend/src/impressive.js'
+          ]
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'angular-frontend/dist/<%= pkg.name %>.js',
+        dest: 'angular-frontend/dist/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+ // grunt.registerTask('default', ['jshint','clean']);
+  grunt.registerTask('default', ['concat', 'uglify']);
+
+};
